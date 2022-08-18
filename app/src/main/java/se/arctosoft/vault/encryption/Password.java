c9718600f0f727a32d9f42a0365c3de235ee62f1
Rikard Teodorsson <9367038+hej2010@users.noreply.github.com>
2022-08-18 15:55:27 +0200

    Removed unused code

--- app/src/main/java/se/arctosoft/vault/encryption/Password.java
+++ app/src/main/java/se/arctosoft/vault/encryption/Password.java
@@ -20,23 +20,6 @@ import se.arctosoft.vault.utils.Settings;
 
 public class Password {
     private static final String TAG = "Password";
-    private static final byte[] SALT = new byte[]{0x66, 0x6d, 0x34, 0x63, 0x6b, 0x41, 0x76, 0x32, 0x63, 0x6d, 0x34, 0x39, 0x35, 0x37, 0x6e, 0x64};
-
-    public static boolean checkPassword(Context context, String pwd) {
-        String s = Settings.getInstance(context).getPasswordHash();
-        try {
-            return calculateHash(pwd).equals(calculateHash(Objects.requireNonNull(s)));
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public static String calculateHash(String s) throws GeneralSecurityException {
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(Encryption.KEY_ALGORITHM);
-        KeySpec keySpec = new PBEKeySpec(s.toCharArray(), SALT, Encryption.ITERATION_COUNT, Encryption.KEY_LENGTH);
-        return new String(secretKeyFactory.generateSecret(keySpec).getEncoded());
-    }
 
     public static void lock(Context context, @NonNull Settings settings) {
         Log.e(TAG, "lock");
